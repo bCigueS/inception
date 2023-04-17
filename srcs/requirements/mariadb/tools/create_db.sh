c3c46cceb937bee611715cf6de4bd1d3a4d8b3a9
Simon Beylot <beylotsimon@gmail.com>
2023-04-17 13:21:43 +0200

    Working

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,6 +1,7 @@
 #!bin/sh
 
 service mysql start;
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';"
@@ -8,4 +9,4 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p${DB_ROOT} shutdown
-exec mysqld_safe &
+exec mysqld_safe 
